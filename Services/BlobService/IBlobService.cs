311ca733e4649e2bcf6a8f634608776dff2cf4c9
20127670 <nonameno1f@gmail.com>
2024-05-20 03:51:12 +0700

    feat(model): adding Blob Azurite Service

---
+++ Services/BlobService/IBlobService.cs
@@ -0,0 +1,9 @@
+﻿namespace MangoStore_API.Services.BlobService
+{
+    public interface IBlobService
+    {
+        Task<string> GetBlob(string blobName, string containerName);
+        Task<bool> DeleteBlob(string blobName, string containerName);
+        Task<string> UploadBlob(string blobName, string containerName, IFormFile file);
+    }
+}
